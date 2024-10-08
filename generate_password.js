1179df56baae12ee7fd2fdd726b0d8b4c73fed86
Brody Broughton <broughton.brody07@gmail.com>
2024-10-07 21:49:16 -0600

    Initial Test

--- generate_password.js
+++ generate_password.js
@@ -1,5 +1,10 @@
 function generatePassword(length = 12) {
-    // complete me
+    const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890_";
+    let password = "";
+    for (let i = 0; i < length; i++) {
+        password += alphabet.charAt(Math.floor(Math.random() * alphabet.length));
+    }
+    return password;
 }
 
 module.exports = generatePassword;
