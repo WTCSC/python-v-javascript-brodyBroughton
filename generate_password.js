2ff9d5726da8dd56677000b04e0c9ccddc513ff4
Brody Broughton <broughton.brody07@gmail.com>
2024-10-07 21:51:56 -0600

    Added Comments

--- generate_password.js
+++ generate_password.js
@@ -1,8 +1,8 @@
 function generatePassword(length = 12) {
-    const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890_";
+    const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890_"; // Bank used for random password
     let password = "";
     for (let i = 0; i < length; i++) {
-        password += alphabet.charAt(Math.floor(Math.random() * alphabet.length));
+        password += alphabet.charAt(Math.floor(Math.random() * alphabet.length)); // Randomly select a character from the alphabet using charAt from Math.random() 
     }
     return password;
 }
